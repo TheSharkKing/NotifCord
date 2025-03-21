2629b5050bbcac48d095296888fd22b696242924
TheSharkKing <27yarberrycd23@gmail.com>
2025-03-21 14:55:47 -0500

    Create twitchstream.py

---
+++ actions/twitchstream.py
@@ -0,0 +1,7 @@
+def announce_stream(user, platform):
+    message = f"{user} has gone live on {platform}!"
+    print(message)
+    # You can add more functionality here, such as sending the message to a chat or logging it
+
+# Example usage
+announce_stream("StreamerName", "Twitch")
