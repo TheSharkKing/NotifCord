f7cb9c0f5fbeb614be7e3809a24eaebcdda5bb93
TheSharkKing <27yarberrycd23@gmail.com>
2025-03-21 15:01:27 -0500

    Edit twitch Add youtube

--- actions/twitchstream.py
+++ actions/twitchstream.py
@@ -1,7 +1,37 @@
-def announce_stream(user, platform):
-    message = f"{user} has gone live on {platform}!"
-    print(message)
-    # You can add more functionality here, such as sending the message to a chat or logging it
+import requests
+import time
 
-# Example usage
-announce_stream("StreamerName", "Twitch")
+# Replace these with your actual Twitch API credentials
+CLIENT_ID = 'your_client_id'
+CLIENT_SECRET = 'your_client_secret'
+ACCESS_TOKEN = 'your_access_token'
+CHANNEL_NAME = 'your_channel_name'
+
+def get_stream_status():
+    url = f'https://api.twitch.tv/helix/streams?user_login={CHANNEL_NAME}'
+    headers = {
+        'Client-ID': CLIENT_ID,
+        'Authorization': f'Bearer {ACCESS_TOKEN}'
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    if data['data']:
+        return True
+    return False
+
+def announce_stream():
+    print(f'{CHANNEL_NAME} is now live on Twitch!')
+
+def main():
+    was_live = False
+    while True:
+        is_live = get_stream_status()
+        if is_live and not was_live:
+            announce_stream()
+            was_live = True
+        elif not is_live:
+            was_live = False
+        time.sleep(60)  # Check every 60 seconds
+
+if __name__ == '__main__':
+    main()
